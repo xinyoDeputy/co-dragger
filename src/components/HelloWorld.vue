b6770ce5ed54de2a8ddeb2fd35bee47e885ea3eb
jchen <jchen@deputy.com>
2022-01-18 09:46:58 +1100

    add horizontal drag

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,37 @@
 <script>
-import { computed, reactive } from "vue";
+import { ref, computed, onMounted, reactive } from "vue";
 import data from "../assets/store.json";
 export default {
   props: {
     msg: String,
     resize: Boolean,
   },
-  setup(props) {
+  setup() {
+    const rosterRef = ref(null);
     const state = reactive({
       ...data,
       dragging: false,
+      draggingX: 0,
+    });
+    onMounted(() => {
+      state.rosterWidth = rosterRef.value.offsetWidth;
+      console.log(state.rosterWidth);
     });
     const areaRow = (areaId) => {
       return state.shifts.filter((x) => x.area == areaId).length;
     };
     const areaHeight = (r) => {
-      return state.rowHeight * (areaRow(r) + 1) + "px";
+      return state.rowHeight * (areaRow(r) + state.leaveSpace) + "px";
+    };
+    const shiftTop = (i) => {
+      return state.rowHeight * i + "px";
+    };
+    const areaShiftSort = (areaId) => {
+      return state.shifts
+        .filter((x) => x.area == areaId)
+        .sort((a, b) => {
+          return a.x - b.x;
+        });
     };
     const empDragStart = (event, emp) => {
       // console.log("empDragStart", emp);
@@ -28,6 +44,7 @@ export default {
       event.dataTransfer.setData("shift", shift.id);
     };
     const onDrop = (event, areaId) => {
+      event.preventDefault();
       if (state.dragging) {
         state.dragging = false;
         return false;
@@ -35,35 +52,55 @@ export default {
       if (event.dataTransfer.getData("shift")) {
         // console.log("shift", event.dataTransfer);
         const shiftId = event.dataTransfer.getData("shift");
-        state.shifts.find((x) => x.id == shiftId).area = areaId;
+        let target = state.shifts.find((x) => x.id == shiftId);
+        target.area = areaId;
+        if (state.draggingX) {
+          let c = state.rosterWidth / 12;
+          target.x = parseInt(state.draggingX / c);
+          state.draggingX = 0;
+        }
       } else if (event.dataTransfer.getData("emp")) {
         var newShift = state.blankShift;
         newShift.emp = event.dataTransfer.getData("emp");
         newShift.area = areaId;
         newShift.id = state.shifts.length;
+        if (state.draggingX) {
+          let c = state.rosterWidth / 12;
+          newShift.x = parseInt(state.draggingX / c);
+          state.draggingX = 0;
+        }
         state.shifts.push({ ...newShift });
         state.areas.find((x) => x.id == areaId).row++;
       } else return;
     };
     const onDropShift = (event, item) => {
-      state.dragging = true;
       const shiftId = event.dataTransfer.getData("shift");
-      console.log(shiftId, "onDropShift swap to", item.id);
+      if (shiftId != item.id) {
+        state.dragging = true;
+        confirm(`${shiftId} is onDropShift swap to ${item.id}`);
+      }
+    };
+    const onDragEnter = (event, area) => {
+      event.preventDefault();
+      // console.log(event.x, event.y, event.offsetX, event.offsetY);
     };
     const onDragOver = (event) => {
-      // console.log("onDragOver", event);
-      // event.dataTransfer.dropEffect = 'copy';
-      // event.dataTransfer.setData('itemID', item.id);
+      event.preventDefault();
+      state.draggingX = event.offsetX;
     };
     return {
       state,
+      rosterRef,
+      areaShiftSort,
       onDrop,
       onDragOver,
+      onDragEnter,
       empDragStart,
       shiftDragStart,
       onDropShift,
       areaRow,
       areaHeight,
+      shiftTop,
     };
   },
 };
@@ -83,34 +120,36 @@ export default {
         {{ item.name }}
       </div>
     </div>
-    <div class="roster-board">
-      <div
-        v-for="area in state.areas"
-        :key="area.id"
-        class="roster co-dragger"
-        :cod-row="area.row"
-        @dragenter.prevent
-        @dragover.prevent
-        @drop="onDrop($event, area.id)"
-        :style="{ height: areaHeight(area.id) }"
-      >
+    <div class="roster-board" ref="rosterRef">
+      <div v-for="area in state.areas" :key="area.id">
+        <div class="area-title">{{ area.name }}</div>
         <div
-          v-for="item in state.shifts.filter((x) => x.area == area.id)"
-          :key="item.id"
-          class="shift-item"
-          :cod-x="item.x"
-          :cod-y="item.y"
-          :cod-h="item.h"
-          :cod-w="item.w"
-          draggable="true"
-          @dragstart="shiftDragStart($event, item)"
-          @drop="onDropShift($event, item)"
-          @dragenter.prevent
-          @dragover.prevent
+          class="roster co-dragger"
+          :cod-row="area.row"
+          @dragenter="onDragEnter($event, area.id)"
+          @dragover="onDragOver($event)"
+          @drop="onDrop($event, area.id)"
+          :style="{ height: areaHeight(area.id) }"
         >
-          <div v-if="resize" class="resize-handle-left"></div>
-          <div v-if="resize" class="resize-handle-right"></div>
-          {{ item.id }} - {{ item.emp }}
+          <div
+            v-for="(item, index) in areaShiftSort(area.id)"
+            :key="item.id"
+            class="shift-item co-dragger-item"
+            :cod-x="item.x"
+            :cod-y="item.y"
+            :cod-h="item.h"
+            :cod-w="item.w"
+            draggable="true"
+            @dragstart="shiftDragStart($event, item)"
+            @drop="onDropShift($event, item)"
+            @dragenter.prevent
+            @dragover.prevent
+            :style="{ top: shiftTop(index) }"
+          >
+            <!-- <div v-if="resize" class="resize-handle-left"></div> -->
+            <!-- <div v-if="resize" class="resize-handle-right"></div> -->
+            shift {{ item.id }} - {{ item.emp }} - start at {{ item.x }}
+          </div>
         </div>
       </div>
     </div>
@@ -125,16 +164,21 @@ export default {
   flex-grow: 1;
 }
 .shift-item {
-  background: moccasin;
+  background-color: moccasin;
   position: relative;
   height: 70px;
   resize: horizontal;
+  transition: left 200ms, top 400ms;
+}
+.shift-item:focus {
+  background: #42b983;
 }
 .emp-item {
   background: paleturquoise;
 }
 .emp,
-.roster {
+.roster,
+.area-title {
   border: 1px solid #ccc;
 }
 a {
